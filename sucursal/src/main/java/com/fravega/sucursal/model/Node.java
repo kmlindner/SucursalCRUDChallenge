041a88d6cf229c3c9d25e34b6bec2d10d8e00e3c
Kenneth <kenneth_la12@hotmail.com>
2022-01-01 16:27:41 -0300

    Se generan clases del modelo y cfg de conexión a BD.

---
+++ sucursal/src/main/java/com/fravega/sucursal/model/Node.java
@@ -0,0 +1,48 @@
+package com.fravega.sucursal.model;
+
+import javax.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "NODE_TYPE", discriminatorType = DiscriminatorType.STRING)
+public abstract class Node {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private double latitude;
+    private double longitude;
+
+    public Node() {
+    }
+
+    public Node(double latitude, double longitude) {
+        this.latitude = latitude;
+        this.longitude = longitude;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+}
