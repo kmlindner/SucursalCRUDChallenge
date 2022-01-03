e23d90724a3c429dcc4cb070220672053cacba2a
Kenneth <kenneth_la12@hotmail.com>
2022-01-02 22:54:30 -0300

    Se genera interfaz y clase para el servicio y controller.

--- sucursal/src/main/java/com/fravega/sucursal/repository/NodeRepository.java
+++ sucursal/src/main/java/com/fravega/sucursal/repository/NodeRepository.java
@@ -2,7 +2,9 @@ package com.fravega.sucursal.repository;
 
 import com.fravega.sucursal.model.Node;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface NodeRepository extends JpaRepository<Node, Integer> {
     
 }
