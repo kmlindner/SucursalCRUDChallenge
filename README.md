30e74acb223d34196feca740be5c840e554c33cb
Kenneth <kenneth_la12@hotmail.com>
2022-01-04 14:01:16 -0300

    Se completa el README y se agrega dump de BD y coleccion de servicios de POSTMAN

--- README.md
+++ README.md
@@ -1,2 +1,50 @@
-# SucursalCRUDChallenge
-API for Sucursal CRUD Challenge
+# Sucursal CRUD API
+
+_Esta API Rest tiene como finalidad realizar las operaciones de alta, baja, modificación y consulta de sucursales (branch offices) y puntos de retiro (withdrawal points). Además informa cual es la sucursal o punto de retiro más cercano a un punto (latitud, longitud) dado._
+
+## Comenzando 🚀
+
+_Para obtener una copia de la aplicación y levantarla en tu entorno local necesitaras tener el cliente de git instalado._
+
+_Luego copiar la url del repositorio del branch main e incluyendola en un git clone. De la siguiente manera:_
+
+```
+git clone https://github.com/kmlindner/SucursalCRUDChallenge.git
+```
+
+_Hecho esto ya tendremos la copia de la app en nuestra computadora._
+
+### Instalación 🔧
+
+_Ahora bien, para que funcione y podamos levantarla de forma exitosa, debemos contar con el siguiente entorno de configuracion:_
+
+```
+Java 8
+Eclipse o IntelliJ
+Maven 3.8.1
+MySQL 8
+```
+
+_Se incluye en la carpeta extra del repo un dump inicial de la base de datos para que se pueda importar su estructura. El nombre de la BD por defecto es fravega. Luego se puede ejecutar el script mencionado anteriormente desde el workbench o DBeaver y ya estaría resuelto._
+
+_Una vez tengamos el software mencionado anteriormente, debemos compilar el proyecto con Maven para que se descarguen todas las dependencias necesarias. Para esto debemos ejecutar un clean install en la ruta del directorio de la app o desde los plugins del IDE seleccionado._
+
+_Si optamos por hacerlo desde consola, tipeamos cmd en la ruta del explorador y cuando se abra la consolta ejecutamos:_
+
+```
+mvn clean install
+```
+
+## Despliegue 📦
+
+_La API esta construida sobre SpringBoot asi que para poder ejecutarla debemos levantar/ejecutar el módulo SucursalApplication. Una vez esté levantada podemos probar la misma desde la documentación construida con Swagger (http://localhost:8080/swagger-api.html) o también desde Postman (Se incluye la colección de servicios en la carpeta extra del repo)._
+
+## Documentación 🛠️
+
+_Una vez levantada la aplicación ingresar al link debajo para acceder a la información sobre los endpoints, requests y responses:_
+
+* [Swagger](http://localhost:8080/swagger-api.html)
+
+## Autores ✒️
+
+* **Kenneth Lindner** _Software Developer_
---
+++ extra/dump-fravega.sql
@@ -0,0 +1,60 @@
+-- MySQL dump 10.13  Distrib 8.0.26, for Win64 (x86_64)
+--
+-- Host: localhost    Database: fravega
+-- ------------------------------------------------------
+-- Server version	8.0.26
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `node`
+--
+
+DROP TABLE IF EXISTS `node`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `node` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `node_type` varchar(31) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NOT NULL,
+  `latitude` double NOT NULL,
+  `longitude` double NOT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `business_hours` varchar(255) DEFAULT NULL,
+  `capacity` bigint DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `node`
+--
+
+LOCK TABLES `node` WRITE;
+/*!40000 ALTER TABLE `node` DISABLE KEYS */;
+INSERT INTO `node` VALUES (1,'BRANCH_OFFICE',45.567,120.567,'Av. Monroe 2465','8:00 - 18:00',NULL),(2,'BRANCH_OFFICE',78.567,110.567,'Av. Corrientes 2465','8:00 - 18:00',NULL),(3,'WITHDRAWAL_POINT',80.567,132.567,NULL,NULL,150000),(4,'WITHDRAWAL_POINT',-34.567,122.567,NULL,NULL,200000);
+/*!40000 ALTER TABLE `node` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'fravega'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-01-04 13:11:41
